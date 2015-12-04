d69aecd751ba38d9295776a528a1f277ca01fb27
George Exarchakos <g.exarchakos@tue.nl>
2015-12-04 10:17:24 +0100

    started a boost.python-independent wrapper of adwin

--- src/util/adwin/adwin++.cpp
+++ src/util/adwin/adwin++.cpp
@@ -1,5 +1,17 @@
 #include "Adwin.h"
 
+extern "C" {
+    Adwin* adwin_create(int M) { return new Adwin(M); }
+    double adwin_estimation(Adwin* win) { return win->getEstimation(); }
+    double adwin_variance(Adwin* win) { return win->getVariance(); }
+    bool adwin_update(Adwin* win, const double &value) { return win->update(value); }
+    void adwin_print(Adwin* win) { win->print(); }
+    int adwin_length(Adwin* win) { return win->length(); }
+}
+
+/**
+ * works only if boost.python is installed. The above ctypes replace the below
+
 #include <boost/python.hpp>
 
 BOOST_PYTHON_MODULE(adwin)
@@ -13,3 +25,4 @@ BOOST_PYTHON_MODULE(adwin)
 		.def("length", &Adwin::length)
     ;
 }
+*/
--- src/util/adwin/adwin.py
+++ src/util/adwin/adwin.py
@@ -1,7 +1,51 @@
-def __bootstrap__():
-   global __bootstrap__, __loader__, __file__
-   import sys, pkg_resources, imp
-   __file__ = pkg_resources.resource_filename(__name__,'adwin.so')
-   __loader__ = None; del __bootstrap__, __loader__
-   imp.load_dynamic(__name__,__file__)
-__bootstrap__()
+import ctypes
+from ctypes import cdll
+import os
+import exceptions
+
+path = os.path.dirname(os.path.realpath(__file__))
+# os.environ['PATH'] = os.path.dirname(__file__) + ';' + os.environ['PATH']
+lib = cdll.LoadLibrary(path + '/libadwin.so')
+
+
+class Adwin(object):
+	# @classmethod
+	# def from_param(cls, obj):
+	# 	if obj is None:
+	# 		raise exceptions.TypeError('Adwin should exist')
+	# 	else:
+	# 		return ctypes.pointer(obj)
+
+	def __init__(self, M):
+		self.obj = lib.adwin_create(M)
+
+	def getEstimation(self):
+		func = lib.adwin_estimation
+		func.restype = ctypes.c_double
+		return func(self.obj)
+
+	def getVariance(self):
+		func = lib.adwin_variance
+		func.restype = ctypes.c_double
+		return func(self.obj)
+
+	def update(self, value):
+		func = lib.adwin_update
+		func.restype = ctypes.c_bool
+		#func.argtypes = [Adwin, ctypes.c_float]
+		return func(ctypes.pointer(self), float(value))
+
+	def printout(self):
+		lib.adwin_print(self.obj)
+
+	def length(self):
+		return lib.adwin_length(self.obj)
+
+		# replaced by the ctypes above. no more need of boost.python library
+		# def __bootstrap__():
+		#    global __bootstrap__, __loader__, __file__
+		#    import sys, pkg_resources, imp
+		#    __file__ = pkg_resources.resource_filename(__name__,'adwin.so')
+		#    __loader__ = None; del __bootstrap__, __loader__
+		#    imp.load_dynamic(__name__,__file__)
+		# __bootstrap__()
